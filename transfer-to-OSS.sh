91587dd02d3374255503b238ab06b4d58ccbffdf
ianlingat <arthlingat@gmail.com>
2023-10-24 17:15:47 +0800

    Update transfer-to-OSS.sh

--- transfer-to-OSS.sh
+++ transfer-to-OSS.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 #/reports/scripts/ossutil64 cp -r /reports/Reports_Passed/ oss://ctvi-bucket/ctvi-reports/
 
+# Set the variable for the log files
+DATE_FILE=`date +"%Y-%m-%d"`
+
 # Set the directory path to check for files
 directory_path="/reports/Reports_Passed"
 
@@ -17,9 +20,11 @@ for file in $files
 do
         if [[ $(ossutil ls oss://ctvi-bucket/ctvi-reports/$file --endpoint https://oss-ap-southeast-6.aliyuncs.com | grep "Object Number is:" | awk '{print $4}') == 0 ]]
         then
-                echo "Proceeding to transfer file $file in OSS Bucket $bucket_name"
-                /reports/scripts/ossutil64 cp /reports/Reports_Passed/$file oss://ctvi-bucket/ctvi-reports/
-        else
-                echo "File $file already exist on OSS Bucket $bucket_name"
+                DATE_IF=`date +"%Y-%m-%d %H:%M:%S"`
+                echo "$DATE_IF Proceeding to transfer file $file in OSS Bucket $bucket_name" >> /reports/transfer-to-oss-logs/transfer-to-oss-v3-$DATE_FILE.log
+                /reports/scripts/ossutil64 cp /reports/Reports_Passed/$file oss://ctvi-bucket/ctvi-reports/ >> /reports/transfer-to-oss-logs/transfer-to-oss-v3-$DATE_FILE.log
+        else 
+                DATE_ELSE=`date +"%Y-%m-%d %H:%M:%S"`
+                echo "$DATE_ELSE File $file already exist on OSS Bucket $bucket_name" >> /reports/transfer-to-oss-logs/transfer-to-oss-v3-$DATE_FILE.log
         fi
 done
