fc76a0abcc330d9b7ac4156778f8024d1846881d
ianlingat <arthlingat@gmail.com>
2023-10-27 14:27:16 +0800

    Update transfer-to-OSS.sh
    Updated 10/27/2023

--- transfer-to-OSS.sh
+++ transfer-to-OSS.sh
@@ -1,5 +1,14 @@
-#!/bin/bash
+!/bin/bash
 #/reports/scripts/ossutil64 cp -r /reports/Reports_Passed/ oss://ctvi-bucket/ctvi-reports/
+
+# Define the lock file path
+lock_file="/reports/scripts/transfer-to-oss-v4.run"
+if [ -e "$lock_file" ]; then
+  echo "Script is already running. Exiting."
+  exit 1
+fi
+
+echo "$$" > "$lock_file"
 
 # Set the variable for the log files
 DATE_FILE=`date +"%Y-%m-%d"`
@@ -15,16 +24,16 @@ endpoint="https://oss-ap-southeast-6.aliyuncs.com"
 
 files=$(ls "$directory_path")
 
-# Loop through each file in the directory
 for file in $files
 do
         if [[ $(ossutil ls oss://ctvi-bucket/ctvi-reports/$file --endpoint https://oss-ap-southeast-6.aliyuncs.com | grep "Object Number is:" | awk '{print $4}') == 0 ]]
         then
                 DATE_IF=`date +"%Y-%m-%d %H:%M:%S"`
-                echo "$DATE_IF Proceeding to transfer file $file in OSS Bucket $bucket_name" >> /reports/transfer-to-oss-logs/transfer-to-oss-v3-$DATE_FILE.log
-                /reports/scripts/ossutil64 cp /reports/Reports_Passed/$file oss://ctvi-bucket/ctvi-reports/ >> /reports/transfer-to-oss-logs/transfer-to-oss-v3-$DATE_FILE.log
-        else 
+                echo "$DATE_IF Proceeding to transfer file $file in OSS Bucket $bucket_name" >> /reports/transfer-to-oss-logs/transfer-to-oss-v4-$DATE_FILE.log
+                /reports/scripts/ossutil64 cp /reports/Reports_Passed/$file oss://ctvi-bucket/ctvi-reports/ >> /reports/transfer-to-oss-logs/transfer-to-oss-v4-$DATE_FILE.log
+        else
                 DATE_ELSE=`date +"%Y-%m-%d %H:%M:%S"`
-                echo "$DATE_ELSE File $file already exist on OSS Bucket $bucket_name" >> /reports/transfer-to-oss-logs/transfer-to-oss-v3-$DATE_FILE.log
+                echo "$DATE_ELSE File $file already exist on OSS Bucket $bucket_name" >> /reports/transfer-to-oss-logs/transfer-to-oss-v4-$DATE_FILE.log
         fi
 done
+rm "$lock_file"
